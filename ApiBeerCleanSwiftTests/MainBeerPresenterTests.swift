d0456d59c4ccfc6dc5c2c0eb3984cf9c772d9300
andrebrilho <andre.txt@hotmail.com>
2020-05-06 16:00:12 -0300

    test in progress

---
+++ ApiBeerCleanSwiftTests/MainBeerPresenterTests.swift
@@ -0,0 +1,9 @@
+//
+//  MainBeerPresenterTests.swift
+//  ApiBeerCleanSwiftTests
+//
+//  Created by André Brilho on 06/05/20.
+//  Copyright © 2020 André Brilho. All rights reserved.
+//
+
+import Foundation
